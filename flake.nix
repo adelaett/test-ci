565881bac88d93048379c67430abce6b68377f75
adelaett <90894311+adelaett@users.noreply.github.com.com>
2022-10-17 17:21:03 +0200

    flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    flake-utils.url = github:numtide/flake-utils;
+    nixpkgs.url = github:NixOS/nixpkgs/nixos-unstable;
+  };
+
+  outputs = {nixpkgs, flake-utils, ...}:
+    let
+      systems = [ "x86_64-linux" ];
+    in flake-utils.lib.eachSystem systems (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+        rec {
+          packages = {
+            hello = pkgs.hello;
+          };
+          defaultPackage = packages.hello;
+          devShell = pkgs.mkShell {
+            inputsFrom = [packages.hello];
+            buildInputs = [
+              pkgs.inotify-tools
+            ];
+          };
+        }
+    );
+}
